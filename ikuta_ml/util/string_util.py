a57ee29eb71c3d5731cda3c8ae80de45b9a3ed94
adtak <adtak62@gmail.com>
2021-05-14 19:03:07 +0900

    feat: update string util to cleanse tweet

--- ikuta_ml/util/string_util.py
+++ ikuta_ml/util/string_util.py
@@ -3,12 +3,17 @@ import re
 import sys
 
 
-def cleanse_tweet(tweet: str):
-    tweet = tweet.replace('\n', '')
+def cleanse_tweet(tweet: str) -> str:
+    tweet = tweet.replace('\n', '。')
     tweet = re.sub(r'https?://[\w/:%#\$&\?\(\)~\.=\+\-…]+', '', tweet)
     tweet = tweet.translate(
         dict.fromkeys(range(0x10000, sys.maxunicode + 1), '。')
     )
-    tweet = emoji.get_emoji_regexp().sub(u'', tweet)
+    tweet = emoji.get_emoji_regexp().sub('。', tweet)
     if tweet.find('#') != -1:
         tweet = tweet[0:tweet.find('#')]
+
+    tweet += '。'
+    tweet = re.sub('。+', '。', tweet)
+
+    return tweet
